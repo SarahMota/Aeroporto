7396fbc20c0adcda0d6d0e9924072a5816cccd76
Roberta <robertarosa.tst@gmail.com>
2023-09-20 23:56:42 -0300

    tentando corrigir os erros que veio da atualização do Yuri, e add minha parte do projeto precisa de ajustes

--- aeroporto/projetoAeroporto.java
+++ aeroporto/projetoAeroporto.java
@@ -1,10 +1,12 @@
-package src.aeroporto;
+package aeroporto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Stack;
+/*import java.util.Queue;
+import java.util.LinkedList;*/
 
-public class projetoAeroporto {
     class Voo {
         String numeroVoo;
         String origem;
@@ -22,7 +24,6 @@ public class projetoAeroporto {
             this.maxPassageiros = maxPassageiros;
         }
     }
-
     class Passageiro {
         String nome;
         int idade;
@@ -41,19 +42,19 @@ public class projetoAeroporto {
         List<Voo> voos;
         boolean adminLogado;
         Stack<Passageiro> passageirosCheckIn;
-    }
-
+    
     public projetoAeroporto() {
         voos = new ArrayList<>();
         adminLogado = false;
         passageirosCheckIn = new Stack<>();
     }
+    
     public void loginComoAdmin() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite a senha do administrador: ");
         if (scanner.hasNextLine()) {
-        String senha = scanner.nextLine();
-        if ("1234".equals(senha)) {
+         String senha = scanner.nextLine();
+         if ("1234".equals(senha)) {
             adminLogado = true;
             System.out.println("Logado como administrador.");
         } else {
@@ -63,5 +64,37 @@ public class projetoAeroporto {
             scanner.close();
         }
     }
+        public void criarVoo(String numeroVoo, String origem, String destino, String horarioPartida, String horarioChegada, int maxPassageiros) {
+        Voo voo = new Voo(numeroVoo, origem, destino, horarioPartida, horarioChegada, maxPassageiros);
+        voos.add(voo);
+        System.out.println("Voo " + numeroVoo + " criado.");
+    }
+
+    public void reservarVoo(String numeroVoo, Passageiro passageiro) {
+        for (Voo voo : voos) {
+            if (voo.numeroVoo.equals(numeroVoo)) {
+                voo.reservasPendentes.add(passageiro);
+                System.out.println("Reserva pendente para o voo " + numeroVoo);
+                return;
+            }
+        }
+        System.out.println("Voo não encontrado.");
+    }
+    public void processarReservas(String numeroVoo) {
+        if (adminLogado) {
+            for (Voo voo : voos) {
+                if (voo.numeroVoo.equals(numeroVoo)) {
+                   voo.passageiros.addAll(voo.reservasPendentes);
+                    voo.reservasPendentes.clear();
+                    System.out.println("Reservas processadas para o voo " + numeroVoo);
+                    return;
+                }
+            }
+            System.out.println("Voo não encontrado.");
+        } else {
+            System.out.println("Login de administrador necessário.");
+        }
+    }
+    
 }
 
